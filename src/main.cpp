f7dd05215b2d4ad9cf74bc7706a317d9798a0386
MichaelReece <s3947763@student.rmit.edu.au>
2023-05-15 15:47:27 +1000

    further setup and basic testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include "../include/village.h"
+#include <mcpp/mcpp.h>
+#include <mcpp/util.h>
+#include <mcpp/block.h>
 
+using namespace mcpp;
 
 int main()
 {
-    
+    mcpp::MinecraftConnection mc;
+
+    mc.postToChat("TEST");
+    Coordinate playerPos = mc.getPlayerPosition();
+    mc.setBlocks(playerPos, playerPos + Coordinate(5, 5, 5), Blocks::STONE);
 }
