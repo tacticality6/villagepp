349078b71ffe21c3f975faa0667110740c51415c
MichaelReece <s3947763@student.rmit.edu.au>
2023-08-02 11:55:40 +1000

    Mostly completed village and house classes

---
+++ src/house.cpp
@@ -0,0 +1,128 @@
+#include <mcpp/mcpp.h>
+#include <mcpp/block.h>
+
+#include <vector>
+#include <random>
+#include <cassert>
+#include <algorithm>
+#include <iterator>
+
+#include "../include/house.h"
+
+#include "../include/plot.h"
+#include "../include/room.h"
+
+using namespace mcpp;
+
+House::House(mcpp::MinecraftConnection* conn, Plot& plot)
+    : plot{plot}
+{
+    assert(conn != nullptr);
+    this->mc = conn;
+
+    this->numRooms = 0;
+    this->numFloors = 0;
+
+    this->l = rand() % (plot.length / 2) + ((plot.length / 2)+1);
+    this->h = 5;
+    this->w = rand() % (plot.width / 2) + ((plot.width / 2));
+
+    this->loc1 = plot.loc1;
+    this->loc2 = Coordinate(plot.loc1.x+l-1, plot.height-1, plot.loc1.z+w-1);
+
+
+    //block types arrays
+    const std::vector<BlockType> structureMats {
+        Blocks::OAK_WOOD_PLANK,
+        Blocks::SPRUCE_WOOD_PLANK,
+        Blocks::BIRCH_WOOD_PLANK,
+        Blocks::JUNGLE_WOOD_PLANK,
+        Blocks::COBBLESTONE,
+        Blocks::STONE_BRICKS,
+        Blocks::NETHER_BRICK,
+        Blocks::SANDSTONE
+    };
+    const std::vector<BlockType> roofSlabMats {
+        Blocks::STONE_SLAB,
+        Blocks::SANDSTONE_SLAB,
+        Blocks::WOODEN_SLAB,
+        Blocks::COBBLESTONE_SLAB,
+        Blocks::BRICK_SLAB,
+        Blocks::STONE_BRICK_SLAB,
+        Blocks::NETHER_BRICK_SLAB,
+        Blocks::QUARTZ_SLAB,
+        Blocks::OAK_WOOD_SLAB,
+        Blocks::SPRUCE_WOOD_SLAB,
+        Blocks::JUNGLE_WOOD_SLAB,
+        Blocks::ACACIA_WOOD_SLAB,
+        Blocks::DARK_OAK_WOOD_SLAB
+    };
+    const std::vector<BlockType> pillarMats {
+        Blocks::OAK_WOOD,
+        Blocks::SPRUCE_WOOD,
+        Blocks::BIRCH_WOOD,
+        Blocks::JUNGLE_WOOD,
+        Blocks::STONE_BRICKS,
+        Blocks::SANDSTONE
+    };
+    const std::vector<BlockType> stairMats {
+        Blocks::OAK_WOOD_STAIRS,
+        Blocks::COBBLESTONE_STAIRS,
+        Blocks::STONE_BRICK_STAIRS,
+        Blocks::NETHER_BRICK_STAIRS,
+        Blocks::SANDSTONE_STAIRS,
+        Blocks::SPRUCE_WOOD_STAIRS,
+        Blocks::JUNGLE_WOOD_STAIRS,
+        Blocks::ACACIA_WOOD_STAIRS
+    };
+
+    //set block types
+    //structure blocks
+    std::vector<BlockType> selectedVec;
+    std::sample(structureMats.begin(), 
+                structureMats.end(), 
+                std::back_inserter(selectedVec), 
+                3, 
+                std::mt19937 {std::random_device{}()}
+               );
+    this->wallMat = selectedVec[0];
+    this->floorMat = selectedVec[1];
+    this->roofMat = selectedVec[2];
+
+    int index = rand() % roofSlabMats.size();
+    this->roofSlabMat = roofSlabMats[index];
+
+    index = rand() % pillarMats.size();
+    this->pillarMat = pillarMats[index];
+
+    index = rand() % stairMats.size();
+    this->stairMat = stairMats[index];
+
+    this->doorType = Blocks::OAK_DOOR_BLOCK;
+}
+
+
+void House::build()
+{
+    this->buildFrame();
+    this->setFrontDoor();
+    this->placeFrontDoor();
+
+    int choiceAxis = (plot.length >= plot.width) ? 0 : 1;
+    this->roomSplit(this->rooms[this->numRooms], choiceAxis);
+
+    bool isMultistorey = (bool)(rand()%2);
+    this->buildFloors(isMultistorey);
+
+    this->buildRoof();
+}
+
+void House::buildFrame()
+{
+    this->mc->setBlocks(loc1, loc2, wallMat);
+}
+
+const Plot& House::getPlot()
+{
+    return plot;
+}
--- src/vilage.cpp
+++ src/vilage.cpp
@@ -1,4 +1,198 @@
 #include <mcpp/mcpp.h>
 
+#include <cassert>
+#include <random>
+
+#include "../include/village.h"
+#include "../include/house.h"
 
 using namespace mcpp;
+
+Village::Village(MinecraftConnection* conn)
+{
+    assert(conn != nullptr);
+    this->mc = conn;
+
+    this->terraformer = Terraformer(this->mc);
+
+    this->numPlots = rand() % 6 + 5; // 5-10
+
+    this->centrepoint = mc->getPlayerPosition();
+
+    Coordinate halfMax {maxSize/2, maxSize/2, maxSize/2};
+    this->villageDomain = {centrepoint-halfMax, centrepoint+halfMax};
+
+    this->arrayRepresentation = {
+        maxSize,
+        std::vector<ArrayFeatures> {
+            maxSize,
+            ArrayFeatures::NONE
+        }
+    };
+    this->updateArrayRepresentationNaturalFeatures();
+}
+
+void Village::updateArrayRepresentationNaturalFeatures()
+{
+    std::vector<std::vector<BlockType>> surfaceBlocks = this->terraformer.getSurfaceBlocks(
+        villageDomain.first, villageDomain.second);
+    
+    for (int x=0; x < surfaceBlocks.size(); x++) {
+        for (int z=0; z < surfaceBlocks[z].size(); z++)
+        {
+            BlockType block = surfaceBlocks[x][z];
+
+            if (block.id == Blocks::STILL_WATER.id
+            || block.id == Blocks::FLOWING_WATER.id
+            || block.id == Blocks::STILL_LAVA.id
+            || block.id == Blocks::FLOWING_LAVA.id) //water
+                arrayRepresentation[x][z] = ArrayFeatures::WATER;
+            else if (arrayRepresentation[x][z] == ArrayFeatures::NONE &&
+            (block.id == Blocks::OAK_LEAVES.id
+             || block.id == Blocks::OAK_WOOD.id)) //tree
+                arrayRepresentation[x][z] = ArrayFeatures::TREE;
+        }
+    }
+}
+
+void Village::setPlots()
+{
+    constexpr int isolationRadius = 10;
+    constexpr int tolerance = 2;
+
+    const std::vector heightArr = this->mc->getHeights(villageDomain.first, villageDomain.second);
+
+    std::vector<std::pair<Coordinate, Coordinate>> interimPlots;
+
+    int flatness = 0;
+    while (plots.size() < numPlots) {
+        for (int x=tolerance; x < arrayRepresentation.size()-tolerance; x++) {
+            for (int z=tolerance; z < arrayRepresentation[x].size()-tolerance; z++)
+            {
+                bool valid = true;
+                for(int l=x-tolerance; l < x+plotSize+tolerance; l++) {
+                    for (int w=z-tolerance; w < z+plotSize+tolerance; w++)
+                    {
+                        if (!(
+                        (
+                            (arrayRepresentation[l][w] == ArrayFeatures::NONE)
+                            ||
+                            (arrayRepresentation[l][w] == ArrayFeatures::TREE)
+                        )
+                        &&
+                        (abs(heightArr[x][z] - heightArr[l][w]) <= flatness)
+                        ))
+                        {
+                            valid = false;
+                            break;
+                        }
+                    }
+                    if (!valid)
+                        break;
+                }
+                if (!valid)
+                    continue;
+                
+
+                //if you get here, plot is valid
+                interimPlots.push_back (
+                    std::pair<Coordinate, Coordinate> {
+                        Coordinate(x,0,z),
+                        Coordinate(x+plotSize,0,z+plotSize)
+                    }
+                );
+
+                //get isolation bounds
+                int xBound = std::max(0, x-isolationRadius);
+                int zBound = std::max(0, z-isolationRadius);
+                //mark array representation
+                //mark isolation radius (temp)
+                for (int l=xBound; l < std::min(x+plotSize+isolationRadius, (int)arrayRepresentation.size()-1); l++) {
+                    for (int w=zBound; z < std::min(z+plotSize+isolationRadius, (int)arrayRepresentation[l].size()-1); w++) {
+                        arrayRepresentation[l][w] = ArrayFeatures::TEMP_ISOLATE;
+                    }
+                }
+
+                //mark plot
+                for (int l=x; l < x+plotSize; x++) {
+                    for (int w=z; w < z+plotSize; z++) {
+                        arrayRepresentation[l][z] = ArrayFeatures::PLOT;
+                    }
+                }
+
+                //exit early if sufficient plots
+                if (plots.size() == numPlots)
+                    break;
+            }
+            //also exit here
+            if (plots.size() == numPlots)
+                break;
+        }
+        //increase flatness tolerance
+        flatness++;
+    }
+
+    //once all plots are place, remove isolation markers
+    for (int x=0; x < arrayRepresentation.size(); x++) {
+        for (int z=0; z < arrayRepresentation[x].size(); z++) {
+            if (arrayRepresentation[x][z] == ArrayFeatures::TEMP_ISOLATE) {
+                arrayRepresentation[x][z] == ArrayFeatures::NONE;
+            }
+        }
+    }
+
+    //update the natural features of the array we just erased with the plots
+    this->updateArrayRepresentationNaturalFeatures();
+}
+
+void Village::build()
+{
+    this->setPlots();
+
+    std::vector<Coordinate> doorCoordinates;
+
+    for (int index=0; index < plots.size(); index++)
+    {
+        if (index<5 || (bool)(rand()%2))
+        {
+            House house = House(mc, plots[index]);
+            //get current far corner
+            Coordinate plotFarCorner = plots[index].loc2;
+            house.build();
+            Coordinate doorCoordinate = house.getDoorCoordinate();
+
+            //update array representation with shrunken plot, and door coordinate
+            Coordinate plotCorner = worldToVillage(plots[index].loc1);
+            for (int x=plotCorner.x; x < plotFarCorner.x; x++) {
+                for (int z=plotCorner.z; z < plotFarCorner.z; z++)
+                {
+                    if (x > plotCorner.x + house.getPlot().length || z > plotCorner.z + house.getPlot().width)
+                        arrayRepresentation[x][z] == ArrayFeatures::NONE;
+                    if (doorCoordinate == villageToWorld(Coordinate(x,plots[index].height,z)))
+                        arrayRepresentation[x][z] == ArrayFeatures::DOOR;
+                }
+            }
+        }
+        else 
+        {
+            //build decoration TODO:
+        }
+    }
+
+    //do roads
+
+}
+
+Coordinate Village::worldToVillage(const Coordinate& loc)
+{
+    return Coordinate(loc.x - villageDomain.first.x, 
+                      loc.y, 
+                      loc.z - villageDomain.first.z);
+}
+
+Coordinate Village::villageToWorld(const Coordinate& loc)
+{
+    return Coordinate(loc.x + villageDomain.first.x, 
+                      loc.y, 
+                      loc.z + villageDomain.first.z);
+}
