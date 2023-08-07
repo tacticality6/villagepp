382cd0d160ec1d80ad0a6d1dbd94c77f22412a7c
MichaelReece <s3947763@student.rmit.edu.au>
2023-08-07 12:06:53 +1000

    created blockMods for handling rotation

---
+++ include/blockMods.h
@@ -0,0 +1,52 @@
+
+namespace BlockMods
+{
+    enum DoorOrient
+    {
+        WEST_TOP = 8,
+        WEST_BOTTOM = 0,
+        NORTH_TOP = 9,
+        NORTH_BOTTOM = 1,
+        EAST_TOP = 10,
+        EAST_BOTTOM = 2,
+        SOUTH_TOP = 11,
+        SOUTH_BOTTOM = 3
+    };
+    enum StairOrient
+    {
+        EAST_ASCENDING = 0,
+        WEST_ASCENDING = 1,
+        NORTH_ASCENDING = 2,
+        SOUTH_ASCENDING = 3,
+        EAST_DESCENDING = 4,
+        WEST_DESCENDING = 5,
+        NORTH_DESCENDING = 6,
+        SOUTH_DESCENDING = 7
+    };
+    enum SignOrient
+    {
+        SOUTH = 0,
+        NORTH = 3,
+        WEST = 5,
+        EAST = 4
+    };
+    enum BedOrient
+    {
+        NORTH_BOTTOM = 0,
+        NORTH_TOP = 8,
+        WEST_BOTTOM = 3,
+        WEST_TOP = 11,
+        SOUTH_BOTTOM = 10,
+        SOUTH_TOP = 2,
+        EAST_BOTTOM = 1,
+        EAST_TOP = 9
+    };
+    enum ButtonOrient
+    {
+        EAST = 2,
+        SOUTH = 4,
+        NORTH = 3,
+        WEST = 1
+    };
+
+}
