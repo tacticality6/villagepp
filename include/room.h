327087d2ede0f52682a033d52be58a92770f9cc8
MichaelReece <s3947763@student.rmit.edu.au>
2023-08-07 12:07:15 +1000

    Implemented room header

--- include/room.h
+++ include/room.h
@@ -1,8 +1,32 @@
 #include <mcpp/mcpp.h>
 
+#include <vector>
+#include <string>
+
 #pragma once
 
 class Room
 {
+private:
+    mcpp::Coordinate l1;
+    mcpp::Coordinate l2;
+    int l;
+    int w;
+    std::vector<int> layout;
+    std::string type;
+    mcpp::Coordinate drCrd;
+
+public:
+    bool isStaircaseInside;
+    bool isFurnished;
+    
+    const mcpp::Coordinate& loc1;
+    const mcpp::Coordinate& loc2;
+    const int& length;
+    const int& width;
+    const mcpp::Coordinate& doorCoord;
+
+    void setLayout();
+    void furnish();
 
 };
