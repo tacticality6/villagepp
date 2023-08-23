dc8685998b1242e6214b3331a3d5342035d745bc
MichaelReece <s3947763@student.rmit.edu.au>
2023-08-23 10:05:13 +1000

    Moved main into try for debugging. Added alternate main for house testing.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "../include/village.h"
 #include "../include/terraformer.h"
+#include "../include/house.h"
 #include <mcpp/mcpp.h>
 #include <mcpp/util.h>
 #include <mcpp/block.h>
@@ -10,11 +11,31 @@ using namespace mcpp;
 
 int main()
 {
-    srand( time(0) );
-    MinecraftConnection mc = MinecraftConnection{};
-    
-    Village village{&mc};
-    village.build();
+    try
+    {
+        
+        srand( time(0) );
+        MinecraftConnection mc = MinecraftConnection{};
+        
+        /*
+        Village village{&mc};
+        village.build();
+        */
+        Coordinate pc = mc.getPlayerPosition() + Coordinate{2,2,2};
+        Coordinate pc2 = pc + Coordinate{14, 14, 14};
+        Terraformer t = Terraformer{&mc};
+        int h = t.flattenPlot(pc, pc2);
+        Plot hp {pc, pc2, h};
+        House house {&mc, hp};
+        house.build();
 
-    return 0;
+        return 0;
+        
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
+    
 }
