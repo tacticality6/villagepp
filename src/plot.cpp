6a2ae31ce0b3fcaf4ee66bc75fe66f94ab51762e
MichaelReece <s3947763@student.rmit.edu.au>
2023-08-24 11:09:37 +1000

    changed plot to remove references

--- src/plot.cpp
+++ src/plot.cpp
@@ -7,16 +7,10 @@ using namespace mcpp;
 Plot::Plot(Coordinate l1, Coordinate l2)
     : Plot(l1, l2, ((l1.y-l2.y)/2))
 {}
-Plot::Plot(Coordinate l1, Coordinate l2, int h)
-    : l1{Coordinate{l1.x, h+1, l1.z}}
-    , l2{Coordinate{l2.x, h+1, l2.z}}
-    , h{h+1}
-    , loc1{this->l1}
-    , loc2{this->l2}
-    , height{this->h}
-    , length{l}
-    , width{w}
-{
-    this->l = abs(loc1.x - loc2.x) + 1;
-    this->w = abs(loc1.z - loc2.z) + 1; 
-}
+Plot::Plot(Coordinate lA, Coordinate lB, int h)
+    : loc1{lA}
+    , loc2{lB}
+    , height{h}
+    , length{abs(loc1.x - loc2.x) + 1}
+    , width{abs(loc1.z - loc2.z) + 1}
+{}
