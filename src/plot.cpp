56c513d06c4212d0d7799a9014fbdf1eaa788aa1
MichaelReece <s3947763@student.rmit.edu.au>
2023-08-24 11:17:49 +1000

    Fix plot height

--- src/plot.cpp
+++ src/plot.cpp
@@ -8,9 +8,9 @@ Plot::Plot(Coordinate l1, Coordinate l2)
     : Plot(l1, l2, ((l1.y-l2.y)/2))
 {}
 Plot::Plot(Coordinate lA, Coordinate lB, int h)
-    : loc1{lA}
-    , loc2{lB}
-    , height{h}
+    : loc1{Coordinate{lA.x, h+1, lA.z}}
+    , loc2{Coordinate{lB.x, h+1, lB.z}}
+    , height{h+1}
     , length{abs(loc1.x - loc2.x) + 1}
     , width{abs(loc1.z - loc2.z) + 1}
 {}
